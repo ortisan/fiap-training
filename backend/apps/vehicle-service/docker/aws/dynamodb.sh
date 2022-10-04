fe136b4cd81c2762eb526f3bd403b1593bebff3d
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2022-10-03 23:33:05 -0300

    Kinesis

--- backend/apps/vehicle-service/docker/aws/dynamodb.sh
+++ backend/apps/vehicle-service/docker/aws/dynamodb.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -x
-stream_arn=$(awslocal dynamodb --endpoint-url=http://localstack:4566 -- create-table \
+awslocal dynamodb --endpoint-url=http://localstack:4566 create-table \
     --table-name Vehicles \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
-        AttributeName=name,AttributeType=S \
-        AttributeName=year,AttributeType=N \
-        AttributeName=model,AttributeType=S \
-        AttributeName=brand,AttributeType=S \
-        AttributeName=licence_plate,AttributeType=S \
+    --stream-specification \
+        StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES \
     --key-schema \
         AttributeName=id,KeyType=HASH \
     --provisioned-throughput \
         ReadCapacityUnits=1,WriteCapacityUnits=1 \
     --query 'TableDescription.LatestStreamArn' \
-    --output text)
+    --output text
 set +x
